cd7fd38be1d8800f5998c5f2ec815ca1fe0a41d6
anurag9012 <88288329+anurag9012@users.noreply.github.com>
2022-04-02 11:38:14 +0530

    adding functionality to keyboard

--- index.js
+++ index.js
@@ -60,3 +60,52 @@ for (let i = 0; i < listofbuttons.length; i++) {
     })
 
 }
+for (let i = 0; i < listofbuttons.length; i++) {
+    listofbuttons[i].addEventListener('keydown', function (event) {
+console.log(event.key)
+        switch (event.key) {
+            case 'w': {
+                crash = new Audio("sounds/crash.mp3")
+                crash.play()
+            }
+                break;
+            case 'a': {
+                tom2 = new Audio("sounds/tom-2.mp3")
+                tom2.play()
+            }
+                break;
+            case 's': {
+                kickbass = new Audio("sounds/kick-bass.mp3")
+                kickbass.play()
+            }
+                break;
+            case 'd': {
+                snare = new Audio("sounds/snare.mp3")
+                snare.play()
+            }
+                break;
+            case 'k': {
+                tom1 = new Audio("sounds/tom-1.mp3")
+                tom1.play()
+            }
+                break;
+            case 'j': {
+                tom3 = new Audio("sounds/tom-3.mp3")
+                tom3.play()
+            }
+                break;
+                case 'l':{
+                    tom4=new Audio("sounds/tom-4.mp3")
+                    tom4.play()
+                }
+                    break;
+
+            default:
+
+                alert("key is not there")
+        }
+
+    })
+
+}
+
